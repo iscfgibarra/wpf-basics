914b0466fc26076ef9260ff84f8f3b0c6531230c
Francisco Ibarra <francisco.ibarra@contpaqi.com>
2024-05-27 17:00:14 -0600

    Agregado el ejemplo de MVVM basico

---
+++ WpfBasics/src/MVVMExamples/MVVM/Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace MVVMExamples.MVVM.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ WpfBasics/src/MVVMExamples/MVVM/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,49 @@
+﻿using MVVMExamples.MVVM.Models;
+using MVVMExamples.Commands;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+namespace MVVMExamples.MVVM.ViewModels;
+
+
+public class PeopleViewModel : INotifyPropertyChanged
+{
+    private ObservableCollection<Person> people;
+
+    public ObservableCollection<Person> People
+    {
+        get => people;
+        set
+        {
+            people = value;
+            OnPropertyChanged(nameof(People));
+        }
+    }
+
+    public ICommand LoadPeopleCommand { get; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public PeopleViewModel()
+    {
+        LoadPeopleCommand = new RelayCommand(_ => LoadPeople());
+    }
+
+    public void LoadPeople()
+    {
+        People = new ObservableCollection<Person>
+        {
+            new Person { Name = "Federico Mendoza", Age = 30 },
+            new Person { Name = "Juana Chana", Age = 25 }
+        };
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+
